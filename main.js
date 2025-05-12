2e488d31db789b57b82483a65025ee2dae45baf1
karizabal <165843581+karizabal@users.noreply.github.com>
2025-05-12 15:14:39 -0700

    add sex and lights legend

--- main.js
+++ main.js
@@ -6,82 +6,123 @@ async function loadData() {
     return data
 }
 
+function renderPlot(data) {
+  // copied this from lecture
+  const width = 1000;
+  const height = 300;
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+  const svg = d3.select('#act-plot');
+  svg.attr('width', width);
+  svg.attr('height', height);
+
+  // create scales
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, data[data.length - 1].hours])
+    .range([margin.left, width - margin.right]);
+
+  const minF = d3.min(data, d => d.favg);
+  const minM = d3.min(data, d => d.mavg);
+  const maxF = d3.max(data, d => d.favg);
+  const maxM = d3.max(data, d => d.mavg);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([
+      Math.min(minF, minM),
+      Math.max(maxF, maxM)
+    ])
+    .range([height - margin.bottom, margin.top]);
+
+  // create axes
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(28)
+
+  const yAxis = d3.axisLeft(yScale);
+
+  // add gridlines to svg plot
+  svg
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
+
+  svg
+    .append('g')
+    .attr('class', 'y axis')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(yAxis);
+
+  // add data to plot
+  const mLine = d3.line()
+    .x(d => xScale(d.hours))
+    .y(d => yScale(d.mavg));
+
+  svg
+    .append('path')
+    .datum(data)
+    .attr('class', 'mavg')
+    .attr('fill', 'none')
+    .attr('stroke', 'royalblue')
+    .attr('stroke-width', 1.5)
+    .attr('d', mLine);
+
+  const fLine = d3.line()
+    .x(d => xScale(d.hours))
+    .y(d => yScale(d.favg));
+
+  svg
+    .append('path')
+    .datum(data)
+    .attr('class', 'favg')
+    .attr('fill', 'none')
+    .attr('stroke', 'pink')
+    .attr('stroke-width', 1.5)
+    .attr('d', fLine);
+}
+
 const data = await loadData();
 console.log(data);
+renderPlot(data);
 
-const svg = d3.select('#act-plot');
-
-// copied this from lecture
-const width = 1000;
-const height = 300;
-const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-
-svg.attr('width', width);
-svg.attr('height', height);
-
-// create scales
-const xScale = d3
-  .scaleLinear()
-  .domain([0, data[data.length - 1].hours])
-  .range([margin.left, width - margin.right]);
-
-const minF = d3.min(data, d => d.favg);
-const minM = d3.min(data, d => d.mavg);
-const maxF = d3.max(data, d => d.favg);
-const maxM = d3.max(data, d => d.mavg);
-
-const yScale = d3
-  .scaleLinear()
-  .domain([
-    Math.min(minF, minM),
-    Math.max(maxF, maxM)
-  ])
-  .range([height - margin.bottom, margin.top]);
-
-// create axes
-const xAxis = d3
-  .axisBottom(xScale)
-  .ticks(28)
-
-const yAxis = d3.axisLeft(yScale);
-
-
-// add gridlines to svg plot
-svg
-  .append('g')
-  .attr('class', 'x axis')
-  .attr('transform', `translate(0, ${height - margin.bottom})`)
-  .call(xAxis);
-
-svg
-  .append('g')
-  .attr('class', 'y axis')
-  .attr('transform', `translate(${margin.left}, 0)`)
-  .call(yAxis);
-
-// add data to plot
-const mLine = d3.line()
-  .x(d => xScale(d.hours))
-  .y(d => yScale(d.mavg));
-
-svg
-  .append('path')
-  .datum(data)
-  .attr('class', 'favg')
-  .attr('fill', 'none')
-  .attr('stroke', 'royalblue')
-  .attr('stroke-width', 1.5)
-  .attr('d', mLine);
-
-const fLine = d3.line()
-  .x(d => xScale(d.hours))
-  .y(d => yScale(d.favg));
-
-svg
-  .append('path')
-  .datum(data)
-  .attr('class', 'favg')
-  .attr('fill', 'none')
-  .attr('stroke', 'pink')
-  .attr('stroke-width', 1.5)
-  .attr('d', fLine);
+const legends = d3.select('#legends');
+const sexLegend = legends.select('#sexes');
+const sexes = ['male', 'female'];
+const sexColors = ['royalblue', 'pink'];
+sexes.forEach((sex, idx) => {
+  sexLegend
+    .append('circle')
+    .attr('cx', 100)
+    .attr('cy', 75 + idx * 20)
+    .attr('r', 5)
+    .style('fill', sexColors[idx])
+  
+  sexLegend
+    .append('text')
+    .attr('x', 110)
+    .attr('y', 75 + idx * 20)
+    .text(sex)
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+});
+const lightLegend = legends.select('#lights');
+const lights = ['on', 'off'];
+const lightColors = ['yellow', 'blue'];
+lights.forEach((sex, idx) => {
+  lightLegend
+    .append('circle')
+    .attr('cx', 100)
+    .attr('cy', 75 + idx * 20)
+    .attr('r', 5)
+    .style('fill', lightColors[idx])
+  
+  lightLegend
+    .append('text')
+    .attr('x', 110)
+    .attr('y', 75 + idx * 20)
+    .text(sex)
+    .style("font-size", "15px")
+    .attr("alignment-baseline","middle")
+});
