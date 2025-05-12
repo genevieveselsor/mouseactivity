217eae11c2c6b02ce6bd2296ed0bb67b355a44ad
karizabal <165843581+karizabal@users.noreply.github.com>
2025-05-11 23:24:05 -0700

    add basic overlaid line plot

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-
 async function loadData() {
-    const rep = await fetch("data.json");
+    const rep = await fetch('data.json');
     const data = await rep.json();
     return data
 }
@@ -10,7 +9,7 @@ async function loadData() {
 const data = await loadData();
 console.log(data);
 
-const svg = d3.select("#act-plot");
+const svg = d3.select('#act-plot');
 
 // copied this from lecture
 const width = 1000;
@@ -21,7 +20,7 @@ svg.attr('width', width);
 svg.attr('height', height);
 
 // create scales
-const XScale = d3
+const xScale = d3
   .scaleLinear()
   .domain([0, data[data.length - 1].hours])
   .range([margin.left, width - margin.right]);
@@ -38,3 +37,51 @@ const yScale = d3
     Math.max(maxF, maxM)
   ])
   .range([height - margin.bottom, margin.top]);
+
+// create axes
+const xAxis = d3
+  .axisBottom(xScale)
+  .ticks(28)
+
+const yAxis = d3.axisLeft(yScale);
+
+
+// add gridlines to svg plot
+svg
+  .append('g')
+  .attr('class', 'x axis')
+  .attr('transform', `translate(0, ${height - margin.bottom})`)
+  .call(xAxis);
+
+svg
+  .append('g')
+  .attr('class', 'y axis')
+  .attr('transform', `translate(${margin.left}, 0)`)
+  .call(yAxis);
+
+// add data to plot
+const mLine = d3.line()
+  .x(d => xScale(d.hours))
+  .y(d => yScale(d.mavg));
+
+svg
+  .append('path')
+  .datum(data)
+  .attr('class', 'favg')
+  .attr('fill', 'none')
+  .attr('stroke', 'royalblue')
+  .attr('stroke-width', 1.5)
+  .attr('d', mLine);
+
+const fLine = d3.line()
+  .x(d => xScale(d.hours))
+  .y(d => yScale(d.favg));
+
+svg
+  .append('path')
+  .datum(data)
+  .attr('class', 'favg')
+  .attr('fill', 'none')
+  .attr('stroke', 'pink')
+  .attr('stroke-width', 1.5)
+  .attr('d', fLine);
